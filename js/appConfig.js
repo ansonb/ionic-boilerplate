df7106a38d619c28b0c2acb6fd9f3813303650f4
ansonb <ansonbastos@gmail.com>
2016-10-22 22:57:58 +0530

    first commit

---
+++ js/appConfig.js
@@ -0,0 +1,15 @@
+(function(){
+	var app = angular.module('bot');
+
+	app.config($stateProvider, $urlRouterProvider){
+
+		// $urlRouterProvider.otherwise(defaultpage);
+
+		// $stateProvider
+		// .state('', {
+		// 	url: '/',
+		// 	templateUrl: '',
+		// 	controller: ''
+		// });
+	}
+})();
